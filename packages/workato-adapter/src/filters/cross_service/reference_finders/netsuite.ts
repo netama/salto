a313c0dd30e7e78433e9ef51acaea661c4b7267d
Neta Marcus <netam.code@gmail.com>
2021-06-03 17:47:30 +0300

    SALTO-1314 netsuite standard objects and fields

--- packages/workato-adapter/src/filters/cross_service/reference_finders/netsuite.ts
+++ packages/workato-adapter/src/filters/cross_service/reference_finders/netsuite.ts
@@ -15,7 +15,7 @@
 */
 import _ from 'lodash'
 import {
-  InstanceElement, ElemID, ReferenceExpression, Values,
+  InstanceElement, ElemID, ReferenceExpression, Values, ObjectType,
 } from '@salto-io/adapter-api'
 import { DependencyDirection } from '@salto-io/adapter-utils'
 import { values as lowerdashValues } from '@salto-io/lowerdash'
@@ -27,6 +27,7 @@ const { isDefined } = lowerdashValues
 
 const OBJECT_REFERENCE_SEEPARATOR = '@@'
 const FIELD_REFERENCE_SEPARATOR = '@'
+const SCRIPT_SUFFIX = 'script'
 
 type NetsuiteFieldMatchGroup = { field: string }
 const isNetsuiteFieldMatchGroup = (val: Values): val is NetsuiteFieldMatchGroup => (
@@ -52,12 +53,22 @@ export const addNetsuiteRecipeReferences = async (
 
     const { dynamicPickListSelection, input } = blockValue
 
-    const addPotentialReference = (value: unknown, separator: string, nestedPath: ElemID): void => {
+    const addPotentialReference = (
+      value: unknown, separator: string, nestedPath: ElemID,
+    ): Readonly<ObjectType> | undefined => {
       const lowercaseSeparator = separator.toLowerCase()
-      if (_.isString(value) && value.split(lowercaseSeparator).length === 2) {
-        const scriptId = _.last(value.toLowerCase().split(lowercaseSeparator))
+      if (!_.isString(value) || value.length === 0) {
+        return undefined
+      }
+      const valueParts = value.toLowerCase().split(lowercaseSeparator)
+      if (valueParts.length > 2) {
+        return undefined
+      }
+      if (valueParts.length === 2 && valueParts[1] !== SCRIPT_SUFFIX) {
+        // TODON look up object type (and return it) when available
+        const scriptId = valueParts[1]
         if (scriptId !== undefined) {
-          const referencedId = indexedElements[scriptId]
+          const referencedId = indexedElements.scriptId[scriptId]
           if (referencedId !== undefined) {
             references.push({
               pathToOverride: nestedPath,
@@ -66,14 +77,44 @@ export const addNetsuiteRecipeReferences = async (
               reference: new ReferenceExpression(referencedId),
             })
           }
+          return undefined
         }
       }
+
+      const referencedElem = indexedElements.type[valueParts[0]]
+      if (referencedElem !== undefined) {
+        references.push({
+          pathToOverride: nestedPath,
+          location: new ReferenceExpression(path),
+          direction: getBlockDependencyDirection(blockValue),
+          reference: new ReferenceExpression(referencedElem.elemID),
+        })
+        return referencedElem
+      }
+
+      return undefined
     }
 
     const netsuiteObject = input?.netsuite_object
     if (netsuiteObject !== undefined) {
-      addPotentialReference(netsuiteObject, OBJECT_REFERENCE_SEEPARATOR, path.createNestedID('input', 'netsuite_object'))
+      const type = addPotentialReference(netsuiteObject, OBJECT_REFERENCE_SEEPARATOR, path.createNestedID('input', 'netsuite_object'))
+      if (type !== undefined) {
+        const inputFieldNames = Object.keys(_.omit(input, 'netsuite_object'))
+        inputFieldNames.forEach(fieldName => {
+          if (type.fields[fieldName] !== undefined) {
+            references.push(
+              {
+                // no pathToOverride because we can't override the field keys in the current format
+                location: new ReferenceExpression(path),
+                direction: getBlockDependencyDirection(blockValue),
+                reference: new ReferenceExpression(type.fields[fieldName].elemID),
+              },
+            )
+          }
+        })
+      }
     }
+
     (dynamicPickListSelection.custom_list ?? []).forEach(({ value }, idx) => {
       addPotentialReference(
         value,
@@ -89,7 +130,7 @@ export const addNetsuiteRecipeReferences = async (
   const formulaReferenceFinder: FormulaReferenceFinder = (value, path) => {
     const potentialFields = formulaFieldMatcher(value).map(match => match.field)
     return potentialFields.map(fieldNameScriptId => {
-      const referencedId = indexedElements[fieldNameScriptId]
+      const referencedId = indexedElements.scriptId[fieldNameScriptId]
       if (referencedId !== undefined) {
         return {
           location: new ReferenceExpression(path),
